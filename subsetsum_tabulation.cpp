d6a28a76e708d656f23d94dadb15bd89a98a7857
ajay0patel <51459396+ajay0patel@users.noreply.github.com>
2021-01-24 11:32:01 +0530

    Create subsetsum_tabulation.cpp

---
+++ subsetsum_tabulation.cpp
@@ -0,0 +1,58 @@
+//wt[] ==> given arr[]
+//w ===> given sum 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int subsetsum(int wt[],int w,int n){
+    int dp[n+1][w+1];
+    
+    for(int i=0;i<=n;i++){
+        dp[i][0]=1;
+    }
+    for(int j=1;j<=w;j++){
+        dp[0][j]=0;
+    }
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=w;j++){
+            
+            if(wt[i-1]>j){
+                dp[i][j] =dp[i-1][j]; 
+             }
+    
+            else if(wt[i-1]<=j){
+                dp[i][j] = dp[i-1][j-wt[i-1]] || dp[i-1][j] ;
+                
+                }
+        }
+        
+    }
+
+    //printing dp array
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=w;j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    
+    return dp[n][w];
+}
+
+
+int main() {
+	
+	int n,w;
+	cin>>n>>w;
+	
+	int wt[n];
+	for(int i=0;i<n;i++){
+	    cin>>wt[i];
+	}
+    
+	subsetsum(wt,w,n) == 1 ? cout<<"YES" : cout<<"NO";
+	
+	return 0;
+}
