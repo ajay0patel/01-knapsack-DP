704eb645ffc46f957fd36ff1dea8d7fd1789ac0d
ajay0patel <51459396+ajay0patel@users.noreply.github.com>
2021-01-24 10:50:07 +0530

    Create subsetsum_recursive.cpp

---
+++ subsetsum_recursive.cpp
@@ -0,0 +1,39 @@
+//return YES if present or NO if not 
+
+#include <bits/stdc++.h>
+using namespace std;
+int subsetsum(int wt[],int w,int n){
+    
+    if(w==0){
+        return 1;
+    }
+    if(n==0){
+        return 0;
+    }
+    
+    else if(wt[n-1]>w){
+        return subsetsum(wt,w,n-1);
+    }
+    
+    else if(wt[n-1]<=w){
+        return subsetsum(wt,w-wt[n-1],n-1) || subsetsum(wt,w,n-1);
+    }   
+}
+
+
+int main() {
+	
+	int n,w;
+	cin>>n>>w;
+	
+	int wt[n];
+	for(int i=0;i<n;i++){
+	    cin>>wt[i];
+	}
+    int dp[n+1][w+1];
+    memset(dp,-1,sizeof(dp));
+    
+	subsetsum(wt,w,n) == 1 ? cout<<"YES" : cout<<"NO";
+	
+	return 0;
+}
